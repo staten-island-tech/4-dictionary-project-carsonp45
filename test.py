07efe04b25a7073c2b6706a2d6f66ba90312acd9
carsonp45 <carsonp3@nycstudents.net>
2026-02-26 10:12:00 -0500

    Day 14

---
+++ test.py
@@ -0,0 +1,11 @@
+
+item = {
+    "name": "El Cinco",
+    "price": 399.99,
+    "department": "Consoles",
+    "description": "With the PlayStation®5 Pro console, the world’s greatest game creators can enhance their games with incredible features like advanced ray tracing, super sharp image clarity for your 4K TV, and high frame rate gameplay*. That means you get to play PS5® games with the most impressive visuals ever possible on a PlayStation® console.PS5® Pro is an all-digital console with no disc drive. Sign into your account for PlayStation and go to PlayStation™Store to buy and download games**.."
+}
+print(item["name"])
+print(item["price"])
+print(item["department"])
+print(item["description"])
